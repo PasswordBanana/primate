3f7ccc5fd5f119f4cb9e0210a48253e5fb1e8fff
Nathan Essex <nathan@essex.id.au>
2014-10-30 23:09:09 +1000

    Added unit tests for filters

--- app/js/filters.js
+++ app/js/filters.js
@@ -22,21 +22,29 @@ primate.filter("passwordStrengthScore", function() {
     };
 });
 
+/*
+ * return a fixed pixel offset if the record is part of a group/subgroup
+ */
 primate.filter("recordIndent", function() {
     return function(record) {
-        return (record.group && record.group !== "") ? 20 : 0;
+        return (record && record.group && record.group !== "") ? 20 : 0;
     };
 });
 
 primate.filter("groupIndent", function() {
     return function(group) {
-        var level = group.level - 1 || 0;
+        if (!group || !group.level) return 0;
+        var level = (Math.floor(group.level) >= 1) ? Math.floor(group.level - 1) : 0;
         return 20 * level;
     }
 });
 
 primate.filter("flagValue", function() {
     return function(tuple) {
-        return checkFlag(tuple[0], tuple[1]);
+        if (tuple && tuple.length === 2 && typeof tuple[0] === "number" && policyFlags.hasOwnProperty(tuple[1])) {
+            return checkFlag(tuple[0], tuple[1]);
+        }
+
+        return false;
     }
 });
